f659c1a0075893134d827537d8eceb071d9a6f80
audahadi <wan.audahadi@gmail.com>
2014-09-23 13:59:57 +0800

    switch back to cm power

--- cancro.mk
+++ cancro.mk
@@ -3,6 +3,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
+# Common QCOM configuration tools
+$(call inherit-product, device/qcom/common/Android.mk)
+
 DEVICE_PACKAGE_OVERLAYS += device/xiaomi/cancro/overlay
 
 LOCAL_PATH := device/xiaomi/cancro
