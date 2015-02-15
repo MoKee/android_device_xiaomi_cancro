3e15e19239e18d0ac3adbac181edd3d7b9f36da4
audahadi <wan.audahadi@gmail.com>
2015-02-15 22:44:18 +0800

    end of trial, remove init.rc

--- cancro.mk
+++ cancro.mk
@@ -26,7 +26,6 @@ PRODUCT_COPY_FILES += \
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/root/fstab.qcom:root/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/root/init.rc:root/init.rc \
     $(LOCAL_PATH)/rootdir/root/init.class_main.sh:root/init.class_main.sh \
     $(LOCAL_PATH)/rootdir/root/init.mdm.sh:root/init.mdm.sh \
     $(LOCAL_PATH)/rootdir/root/init.qcom.class_core.sh:root/init.qcom.class_core.sh \
