8fc0f343ab533185920a34e76682b17a6dc0aa35
pbr1111 <pbr1111@gmail.com>
2015-02-23 09:27:18 +0100

    Needed for thermal-perf

--- cancro.mk
+++ cancro.mk
@@ -161,6 +161,7 @@ PRODUCT_COPY_FILES += \
 # Thermal config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine-8974.conf \
+    $(LOCAL_PATH)/configs/thermal-engine-perf.conf:system/etc/thermal-engine-perf.conf
 
 # Proprietery Firmware
 PRODUCT_COPY_FILES += \
