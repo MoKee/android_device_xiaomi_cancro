06369c6c49abd1c72601612b25c5f781b0906b1d
audahadi <wan.audahadi@gmail.com>
2015-12-08 03:57:27 +0800

    set bluetooth name to cancro

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -24,7 +24,7 @@
 
 #define BLUETOOTH_QCOM_SW TRUE
 
-#define BTM_DEF_LOCAL_NAME   "Xiaomi MI3W"
+#define BTM_DEF_LOCAL_NAME   "cancro"
 // Enables Interleave scan
 #define BTA_HOST_INTERLEAVE_SEARCH  TRUE
 // Disables read remote device feature
