d9d5ab3b5e53831c485ff66b3ddb430afd840d84
audahadi <wan.audahadi@gmail.com>
2015-12-29 13:11:58 -0500

    kernel : switch back to "arm-eabi"
    Change-Id: I633a1ef34d9aef2f5a9325024d692d8fde516168

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,7 @@ USE_CLANG_PLATFORM_BUILD := true
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 
 # Kernel
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 BOARD_KERNEL_CMDLINE := console=none vmalloc=340M androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_DTBTOOL_ARGS := -2
