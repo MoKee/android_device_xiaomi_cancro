0161d2adb8a3bca4a0652d064c815ff2f69b9497
Simon Shields <simon@lineageos.org>
2017-08-23 01:01:40 +1000

    cancro: init: use libbase for log headers
    Change-Id: I05618bd1de934cc41790d77189bd5971e417c96d

--- init/Android.mk
+++ init/Android.mk
@@ -5,6 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
+LOCAL_STATIC_LIBRARIES := libbase
 LOCAL_SRC_FILES := init_cancro.cpp
 LOCAL_MODULE := libinit_cancro
 
