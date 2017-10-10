8484442bc88a823cbcd9da30303ed7a1ea163d66
Hemant Sharma <hemantbeast@gmail.com>
2017-10-10 23:16:44 +0530

    cancro: Extend seccomp policy
    Change-Id: If8a9d5d609fd749c83890b25b58f6707a7c3b659

--- cancro.mk
+++ cancro.mk
@@ -263,7 +263,8 @@ PRODUCT_PACKAGES += \
     
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
