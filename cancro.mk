cd6d7e25af72fa657a1eba52d1d047915bae45e6
GuaiYiHu <guaiyihu@foxmail.com>
2017-10-13 23:59:18 +0800

    cancro: Add missing Seccomp policy
    Change-Id: I313737bbe5483c9ea46e30071cf09913df4ae0e8

--- cancro.mk
+++ cancro.mk
@@ -259,6 +259,10 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libnl_2 \
     libbson
+    
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
 PRODUCT_PACKAGES += \
