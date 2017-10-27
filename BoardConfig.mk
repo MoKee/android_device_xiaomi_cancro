21a498f3c069b97e878c52a22768b5772403f511
GuaiYiHu <guaiyihu@foxmail.com>
2017-10-27 18:27:37 +0800

    cancro: Set BOARD_KERNEL_IMAGE_NAME
    Change-Id: Ie921095ab6cc2cffc062f41c54f2b1e690a9fa39

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,7 @@ BOARD_KERNEL_CMDLINE               := console=none vmalloc=340M androidboot.hard
 BOARD_KERNEL_SEPARATED_DT          := true
 BOARD_KERNEL_BASE                  := 0x00000000
 BOARD_KERNEL_PAGESIZE              := 2048
+BOARD_KERNEL_IMAGE_NAME            := zImage
 BOARD_MKBOOTIMG_ARGS               := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000
 TARGET_KERNEL_SOURCE               := kernel/xiaomi/virgo
 TARGET_KERNEL_ARCH                 := arm
