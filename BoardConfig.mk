64216475876924fbc749623e6170d37f651a29c9
GuaiYiHu <guaiyihu@foxmail.com>
2017-11-03 17:52:58 +0800

    cancro: Use half-res boot ani
    Change-Id: Ie6b80cf3a1ba93a23055f625f35e6d53e2f6597e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,9 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(CANCRO_PATH)/bluetooth
 QCOM_BT_USE_SMD_TTY                         := true
 BLUETOOTH_HCI_USE_MCT                       := true
 
+# Boot animation
+TARGET_BOOTANIMATION_HALF_RES := true
+
 # Radio
 TARGET_RIL_VARIANT                := caf
 FEATURE_QCRIL_UIM_SAP_SERVER_MODE := true
