8a2fa60b93072eb9ea6b7802c71a3c5d0ffd0ac3
Artem Borisov <dedsa2002@gmail.com>
2017-11-09 17:45:37 +0300

    cancro: enable binderized gralloc service
    There is no reason to keep using passthrough, as our binder works properly now.
    
    Change-Id: I9b72e9e703cfccc1c9a21f39264412cda0a6e233

--- cancro.mk
+++ cancro.mk
@@ -232,6 +232,7 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libgenlock \
     android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
