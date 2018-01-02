948705ae53cc0c7b88bf4e836f720e38b5d9d908
GuaiYiHu <guaiyihu@foxmail.com>
2018-01-03 00:53:53 +0800

    cancro: Add TWRP Support
    To build:
    
      * Clone https://github.com/omnirom/android_bootable_recovery/
        to bootable/recovery-twrp (make sure to use the corresponding Android version branch, i.e. android-7.1 for cm-14.1)
    
      * (Optional) If you wish to use BusyBox for wider tool compatibllity, clone a buildable and functional Android 7.1.2 busybox
        to external/busybox, or use a prebuilt and add the corresponding PRODUCT_COPY_FILE to /system/xbin/ where TWRP expects it, then comment TW_USE_TOOLBOX in twrp.mk. -- I opted to use ToolBox by defualt because I couldn't find a buildable/functional busybox for 7.1.2 trees (they all fail to build, including LineageOS's repo).
    
      * WITH_TWRP=true mka recoveryimage
    
    Change-Id: I102b3a6536f34d9a8734ec3767149981f587f0c9

---
+++ twrp/twrp.mk
@@ -0,0 +1,18 @@
+# TWRP
+BOARD_HAS_NO_REAL_SDCARD                        := true
+BOARD_RECOVERY_SWIPE                            := true
+BOARD_SUPPRESS_SECURE_ERASE                     := true
+BOARD_SUPPRESS_EMMC_WIPE                        := true
+RECOVERY_SDCARD_ON_DATA				:= true
+RECOVERY_GRAPHICS_USE_LINELENGTH	        := true
+TARGET_RECOVERY_QCOM_RTC_FIX                    := true
+TW_FLASH_FROM_STORAGE				:= true
+TW_INTERNAL_STORAGE_PATH			:= "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT		        := "data"
+TW_TARGET_USES_QCOM_BSP				:= true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID          := true
+TW_NO_LEGACY_PROPS                              := true
+TW_NO_SCREEN_TIMEOUT                            := true
+TW_NO_SCREEN_BLANK                              := true
+
+PRODUCT_COPY_FILES += device/xiaomi/virgo/twrp/twrp.fstab:recovery/root/etc/twrp.fstab
