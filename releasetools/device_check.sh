8eb26097f9232ba18ba1c7a2bf959f646739d096
GuaiYiHu <guaiyihu@foxmail.com>
2018-01-17 16:53:34 +0800

    cancro: Remove consumerir support for Mi3
    Change-Id: Ic5fa92fa18c54f326815084b57924ac8f1eb1c34

--- releasetools/device_check.sh
+++ releasetools/device_check.sh
@@ -35,6 +35,11 @@ if [ $RAW_ID == 1974 ] || [ $RAW_ID == 1972 ]; then
     rm -f /system/vendor/etc/diracmobile.config
     mv /system/vendor/etc/diracmobile_4.config /system/vendor/etc/diracmobile.config
 else
+    # Remove Mi4 consumerir support
+    rm -rf /system/etc/permissions/android.hardware.consumerir.xml
+    rm -rf /system/vendor/lib/hw/consumerir.msm8974.so
+    rm -rf /system/vendor/lib/hw/android.hardware.ir@*.so
+    rm -rf /system/vendor/bin/hw/android.hardware.ir@*.so
     # Remove Mi4 audio configs
     rm -rf /system/etc/acdbdata/MTP/MTP_Speaker_cal_4.acdb
     rm -f /system/vendor/etc/mixer_paths_4.xml
