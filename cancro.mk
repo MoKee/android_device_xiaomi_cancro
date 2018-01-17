d0aa512fdbf7603b166caa1f21b9180134bc1e9d
Michael Bestas <mkbestas@lineageos.org>
2018-01-17 18:59:09 +0200

    cancro: Move sec_config to vendor
    Change-Id: Ie5f7c8f96243a4ed354f87d78ede9176a46b44be

--- cancro.mk
+++ cancro.mk
@@ -113,7 +113,7 @@ PRODUCT_PACKAGES += \
 
 # IPC router config
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:system/vendor/etc/sec_config
 
 # IR
 PRODUCT_PACKAGES += \
