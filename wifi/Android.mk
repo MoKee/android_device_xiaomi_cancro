b4e0bbaa5e97f169e4bfac037703c26caa8ea978
GuaiYiHu <guaiyihu@foxmail.com>
2018-02-11 16:57:50 +0800

    cancro: wifi: Split libwcnss_qmi
    Change-Id: Ic06eaf7cf5a22f81e8c0d4ca764aedf89bdfbde7

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -20,11 +20,4 @@ LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE           := libwcnss_qmi
-LOCAL_MODULE_TAGS      := optional
-LOCAL_C_INCLUDES       += hardware/qcom/wlan/wcnss_service
-LOCAL_SRC_FILES        := wcnss_xiaomi_client.c
-LOCAL_CFLAGS           += -Wall
-LOCAL_SHARED_LIBRARIES := libc libcutils libutils liblog libqminvapi
 endif
