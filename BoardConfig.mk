2d5fc53edcf42c21bb50c174318bfc2c4a8bb0ca
GuaiYiHu <guaiyihu@foxmail.com>
2018-03-02 22:29:38 +0800

    cancro: Remove secdiscard command
    Change-Id: Ie74ddc5d4e10ee6b97c8f3ec547d472f7fa79bf2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,6 +197,9 @@ TARGET_LD_SHIM_LIBS := /system/lib/libcutils.so|libshim_atomic.so:/system/vendor
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
+# Remove secdiscard command
+TARGET_REMOVE_SECDISCARD_COMMAND := true
+
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
   ifeq ($(TARGET_BUILD_VARIANT),user)
