eba3c06f6ed509a783a9efc849c316599168bda9
GuaiYiHu <guaiyihu@foxmail.com>
2018-03-21 12:51:33 +0800

    cancro: Switch to Mokee's custom vibrator HAL
    Change-Id: I46b11025c8de829cf137ae4aed31959131f5c6da

--- cancro.mk
+++ cancro.mk
@@ -72,7 +72,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.mokee
 
 #USB HAL
 PRODUCT_PACKAGES += \
