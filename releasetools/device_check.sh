d11360aecb6237770748982ac5497fb627738fec
GuaiYiHu <guaiyihu@foxmail.com>
2019-09-18 20:04:42 +0800

    cancro: Update releasetools for system as root
    Change-Id: Ic8ef4d4864c543e87449a161bffdd53d02459bff

--- releasetools/device_check.sh
+++ releasetools/device_check.sh
@@ -20,32 +20,32 @@ RAW_ID=`cat /sys/devices/system/soc/soc0/raw_id`
 
 if [ $RAW_ID == 1974 ] || [ $RAW_ID == 1972 ]; then
     # Remove NFC
-    rm -rf /system/app/NfcNci
-    rm -rf /system/priv-app/Tag
-    rm -rf /system/lib/*nfc*
-    rm -rf /system/etc/*nfc*
-    rm -rf /system/vendor/etc/permissions/*nfc*
-    rm -rf /system/vendor/firmware/bcm2079x-b5_firmware.ncd
-    rm -rf /system/vendor/firmware/bcm2079x-b5_pre_firmware.ncd
-    rm -rf /system/vendor/lib/hw/android.hardware.nfc@1.0-impl-bcm.so
+    rm -rf /system/system/app/NfcNci
+    rm -rf /system/system/priv-app/Tag
+    rm -rf /system/system/lib/*nfc*
+    rm -rf /system/system/etc/*nfc*
+    rm -rf /system/system/vendor/etc/permissions/*nfc*
+    rm -rf /system/system/vendor/firmware/bcm2079x-b5_firmware.ncd
+    rm -rf /system/system/vendor/firmware/bcm2079x-b5_pre_firmware.ncd
+    rm -rf /system/system/vendor/lib/hw/android.hardware.nfc@1.0-impl-bcm.so
     # Use Mi4 audio configs
-    rm -f /system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb
-    mv /system/etc/acdbdata/MTP/MTP_Speaker_cal_4.acdb /system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb
-    rm -f /system/vendor/etc/mixer_paths.xml
-    mv /system/vendor/etc/mixer_paths_4.xml /system/vendor/etc/mixer_paths.xml
+    rm -f /system/system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb
+    mv /system/system/etc/acdbdata/MTP/MTP_Speaker_cal_4.acdb /system/system/etc/acdbdata/MTP/MTP_Speaker_cal.acdb
+    rm -f /system/system/vendor/etc/mixer_paths.xml
+    mv /system/system/vendor/etc/mixer_paths_4.xml /system/system/vendor/etc/mixer_paths.xml
     # Mi4 libdirac config
-    rm -f /system/vendor/etc/diracmobile.config
-    mv /system/vendor/etc/diracmobile_4.config /system/vendor/etc/diracmobile.config
+    rm -f /system/system/vendor/etc/diracmobile.config
+    mv /system/system/vendor/etc/diracmobile_4.config /system/system/vendor/etc/diracmobile.config
 else
     # Remove Mi4 consumerir support
-    rm -rf /system/vendor/etc/permissions/android.hardware.consumerir.xml
-    rm -rf /system/vendor/bin/hw/android.hardware.ir@1.0-service.cancro
-    rm -rf /system/vendor/etc/init/android.hardware.ir@1.0-service.cancro.rc
+    rm -rf /system/system/vendor/etc/permissions/android.hardware.consumerir.xml
+    rm -rf /system/system/vendor/bin/hw/android.hardware.ir@1.0-service.cancro
+    rm -rf /system/system/vendor/etc/init/android.hardware.ir@1.0-service.cancro.rc
     # Remove Mi4 audio configs
-    rm -rf /system/etc/acdbdata/MTP/MTP_Speaker_cal_4.acdb
-    rm -f /system/vendor/etc/mixer_paths_4.xml
+    rm -rf /system/system/etc/acdbdata/MTP/MTP_Speaker_cal_4.acdb
+    rm -f /system/system/vendor/etc/mixer_paths_4.xml
     # Remove Mi4 libdirac config
-    rm -f /system/vendor/etc/diracmobile_4.config
+    rm -f /system/system/vendor/etc/diracmobile_4.config
 fi
 
 if [ $RAW_ID == 1978 ] || [ $RAW_ID == 1972 ] || [ $RAW_ID == 1974 ]; then
